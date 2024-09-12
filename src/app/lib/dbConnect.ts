ed02a1b77f4ef5eeab9b4f073fdd8d05dfe16774
Morswin <morswin@test.pl>
2024-09-12 19:49:48 +0200

    feat: [PK-183] connect next app to mongodb, create first api routes, list data on page

---
+++ src/app/lib/dbConnect.ts
@@ -0,0 +1,18 @@
+import { config } from 'dotenv';
+import mongoose from 'mongoose';
+
+const connection: { isConnected?: number} = {};
+
+config();
+
+async function dbConnect(){
+  if(connection.isConnected){
+    return
+  };
+
+  const db = await mongoose.connect(process.env.MONGODB_URI as string);
+
+  connection.isConnected = db.connections[0].readyState;
+}
+
+export default dbConnect;
