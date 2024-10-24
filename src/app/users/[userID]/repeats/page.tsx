a772f393629608e83898c2493fe3c9ce4573cc16
Morswin <morswin@test.pl>
2024-10-24 18:10:37 +0200

    PK-224 add repeats view

--- src/app/users/[userID]/repeats/page.tsx
+++ src/app/users/[userID]/repeats/page.tsx
@@ -1,7 +1,13 @@
-export default async function UserRepeats() {
+import { getUserWords } from "@/app/requests/getUserWords";
+import { Box } from "@mui/material";
+import { Repeats } from "./repeats";
+
+export default async function UserRepeats({ params }: { params: { userID: string } }) {
+	const words = await getUserWords(params.userID);
+
 	return (
-		<div className="min-h-screen items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
-			repeats
-		</div>
+		<Box className="min-h-screen items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
+			<Repeats words={words} />
+		</Box>
 	);
 }
