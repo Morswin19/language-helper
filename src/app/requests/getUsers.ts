038ca509b7177273ed58778e71fb5c96f529c374
Morswin <morswin@test.pl>
2024-11-01 10:55:44 +0100

    change users page to dynamic

--- src/app/requests/getUsers.ts
+++ src/app/requests/getUsers.ts
@@ -3,7 +3,8 @@ import { User } from "../models/User";
 export const getUsers = async (): Promise<User[]> => {
 	const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 	const res = await fetch(`${baseUrl}/api/users`, {
-		next: { revalidate: 5 },
+		// next: { revalidate: 5 },
+		cache: "no-store",
 	});
 	const jsonRes = await res.json();
 	return jsonRes.users;
