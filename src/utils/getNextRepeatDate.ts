929705282dc89f16555cb0fb4a2647ba299eea3a
Piotr Kalman <pkalman@katalogmarzen.pl>
2025-07-13 18:08:31 +0200

    feat: [PK-227] Change next repeat date depending on streak value

--- src/utils/getNextRepeatDate.ts
+++ src/utils/getNextRepeatDate.ts
@@ -1,18 +1,44 @@
-export const getNextRepeatDate = (repeats: number, goodRepeats: number, badRepeats: number) => {
-	const percentOfGoodRepeats = (goodRepeats * 100) / repeats;
-
+export const getNextRepeatDate = (streak: number) => {
 	const nextRepeatDate = new Date();
 
-	if (percentOfGoodRepeats < 20) {
-		nextRepeatDate.setDate(nextRepeatDate.getDate() + 1); // add 1 day
-	} else if (percentOfGoodRepeats < 40) {
-		nextRepeatDate.setDate(nextRepeatDate.getDate() + 3); // add 3 days
-	} else if (percentOfGoodRepeats < 60) {
-		nextRepeatDate.setDate(nextRepeatDate.getDate() + 7); // add 1 week
-	} else if (percentOfGoodRepeats < 80) {
-		nextRepeatDate.setDate(nextRepeatDate.getDate() + 14); // add 2 weeks
-	} else {
-		nextRepeatDate.setMonth(nextRepeatDate.getMonth() + 1); // add 1 month
+	switch (streak) {
+		case 0:
+			nextRepeatDate.setDate(nextRepeatDate.getDate() + 2); // 2 days
+			break;
+		case 1:
+			nextRepeatDate.setDate(nextRepeatDate.getDate() + 7); // 1 week
+			break;
+		case 2:
+			nextRepeatDate.setDate(nextRepeatDate.getDate() + 14); // 2 weeks
+			break;
+		case 3:
+			nextRepeatDate.setDate(nextRepeatDate.getDate() + 21); // 3 weeks
+			break;
+		case 4:
+			nextRepeatDate.setDate(nextRepeatDate.getDate() + 28); // 4 weeks
+			break;
+		case 5:
+			nextRepeatDate.setDate(nextRepeatDate.getDate() + 35); // 5 weeks
+			break;
+		case 6:
+			nextRepeatDate.setDate(nextRepeatDate.getDate() + 42); // 6 weeks
+			break;
+		case 7:
+			nextRepeatDate.setDate(nextRepeatDate.getDate() + 49); // 7 weeks
+			break;
+		case 8:
+			nextRepeatDate.setDate(nextRepeatDate.getDate() + 56); // 8 weeks
+			break;
+		case 9:
+			nextRepeatDate.setDate(nextRepeatDate.getDate() + 63); // 9 weeks
+			break;
+		case 10:
+			nextRepeatDate.setDate(nextRepeatDate.getDate() + 70); // 10 weeks
+			break;
+		default:
+			// For streaks > 10, cap at 10 weeks
+			nextRepeatDate.setDate(nextRepeatDate.getDate() + 70);
+			break;
 	}
 
 	return nextRepeatDate;
